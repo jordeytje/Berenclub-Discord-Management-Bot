2411186b231731a93a3022711096572dbb50eee5
Spoochy <jordeytje@live.nl>
2022-02-10 22:53:00 +0100

    code cleanup

--- commands/bot_talk.js
+++ commands/bot_talk.js
@@ -1,24 +1,21 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-	name: 'speak',
+	name: 'say',
+    aliases: ['speak'],
 	description: "Praat als bot",
 	async execute(client, message, cmd, args) {
-    if (message.author.id === process.env.DEV_ID) {
-      console.log(args);
-      if (args[0] && args[1]) {
-        let text = args.slice(1).join();
-        console.log(text);
-        text = text.replace(/,/g, ' ');
+        if (message.author.id === process.env.DEV_ID && args[0] && args[1]) {
+            let text = args.slice(1).join();
+            text = text.replace(/,/g, ' ');
 
-        const embed = new MessageEmbed()
-          .setColor('#13ec9c')
-          .setTitle(`${args[0]}`)
-          .setDescription(`${text}`)
-          .setTimestamp();
+            const embed = new MessageEmbed()
+                .setColor('#13ec9c')
+                .setTitle(`${args[0]}`)
+                .setDescription(`${text}`)
+                .setTimestamp();
 
-        message.channel.send(embed);
-      }
+            message.channel.send(embed);
+        }
     }
-	}
 };
--- commands/music_month.js
+++ commands/music_month.js
@@ -5,44 +5,43 @@ module.exports = {
 	aliases: [ 'tmm'],
 	description: "De Tycho muziek maand - December 2021",
 	async execute(client, message, cmd, args) {
-		const today = new Date();
-    const day = parseInt(today.getDate());
-    const month = today.toLocaleString('default', { month: 'long' });
+        const today = new Date();
+        const day = parseInt(today.getDate());
+        const month = today.toLocaleString('default', { month: 'long' });
+        const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-		const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+        const randomizeMusic = (input) => {
+          const arr = musicData[`${alphabet[input]}`];
+          let random = arr[Math.floor(Math.random()*arr.length)];
 
-    const randomizeMusic = (input) => {
-      const arr = musicData[`${alphabet[input]}`]
-      let random = arr[Math.floor(Math.random()*arr.length)];
+          message.channel.send(`${message.author} Jouw genre voor vandaag is: **${random}**.`);
+        };
 
-       message.channel.send(`${message.author} Jouw genre voor vandaag is: **${random}**.`);
-    }
+        if (args.length > 0) {
+          if (args[0] === 'r') {
+              let random = Math.floor(Math.random() * 26);
+              message.channel.send(`${message.author} Is feeling funky en heeft random de letter **${alphabet[random]}** gekozen. Vandaag ga jij luisteren naar...`);
 
-    if (args.length > 0) {
-      if (args[0] === 'r') {
-          let random = Math.floor(Math.random() * 26);
-          message.channel.send(`${message.author} Is feeling funky en heeft random de letter **${alphabet[random]}** gekozen. Vandaag ga jij luisteren naar...`);
+              return randomizeMusic(random);
+          }
 
-          return randomizeMusic(random);
-      }
+          args = parseInt(args[0]);
 
-      args = parseInt(args[0]);
+          if (args > 0 && args <= 26) {
+              message.channel.send(`${message.author} Je hebt gekozen voor dag **${args}**. De dag met letter **${alphabet[args - 1]}** Vandaag ga jij luisteren naar...`);
+              return randomizeMusic(args - 1);
+          } else {
+            message.channel.send(`${message.author} Je bent een incapabele ziel en het alfabet is maar 26 tekens lang. Omdat je geen normale argumenten mee kan geven krijg je de lijst van vandaag. Vandaag is het **${day} ${month}**. Vandaag is de letter **${alphabet[day]}** en ga je luisteren naar...`);
+            return randomizeMusic(day);
+          }
 
-      if (args > 0 && args <= 26) {
-          message.channel.send(`${message.author} Je hebt gekozen voor dag **${args}**. De dag met letter **${alphabet[args - 1]}** Vandaag ga jij luisteren naar...`);
-          return randomizeMusic(args - 1);
-      } else {
-        message.channel.send(`${message.author} Je bent een incapabele ziel en het alfabet is maar 26 tekens lang. Omdat je geen normale argumenten mee kan geven krijg je de lijst van vandaag. Vandaag is het **${day} ${month}**. Vandaag is de letter **${alphabet[day]}** en ga je luisteren naar...`);
-        return randomizeMusic(day);
-      }
-
-    } else {
-      if (day > 0 && day <= 26) {
-        message.channel.send(`${message.author} Vandaag is het **${day} ${month}**. Vandaag is de letter **${alphabet[day - 1]}** en ga je luisteren naar...`);
-        return randomizeMusic(day - 1);
-      } else {
-        message.channel.send(`${message.author} Vandaag is het **${day} ${month}**. Vandaag is er geen letter. Maar je kan zelf een dag kiezen door een nummer mee te geven.`);
-      }
-    }
+        } else {
+          if (day > 0 && day <= 26) {
+            message.channel.send(`${message.author} Vandaag is het **${day} ${month}**. Vandaag is de letter **${alphabet[day - 1]}** en ga je luisteren naar...`);
+            return randomizeMusic(day - 1);
+          } else {
+            message.channel.send(`${message.author} Vandaag is het **${day} ${month}**. Vandaag is er geen letter. Maar je kan zelf een dag kiezen door een nummer mee te geven.`);
+          }
+        }
 	}
 };
